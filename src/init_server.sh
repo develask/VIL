e1480b250ba5832ab1f7e1ad28ca9aa1ed60a8c1
alopez247 <alopez247@hotmail.com>
2017-03-07 16:20:57 +0100

    Prepared the pseudocode in manta_main.py. Take a look and think how should we implement it.

--- src/init_server.sh
+++ src/init_server.sh
@@ -1,5 +1,5 @@
 # initialize simulation with graphics. add -h to disable graphics.
 PATH_TO_VREP="/opt/V-REP_PRO_EDU_V3_3_2_64_Linux"
 
-sh $PATH_TO_VREP/vrep.sh -gREMOTEAPISERVERSERVICE_19999_TRUE_FALSE ../scenes/circuito0.ttt
+sh $PATH_TO_VREP/vrep.sh -h -gREMOTEAPISERVERSERVICE_19999_TRUE_FALSE ../scenes/circuito0.ttt
 
