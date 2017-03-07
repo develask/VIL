ccd84b902354351b349f7800ccf5619f9351393c
alopez247 <alopez247@hotmail.com>
2017-03-07 21:05:14 +0100

    choose_action and other necessary functions added

--- src/class_Manta.py
+++ src/class_Manta.py
@@ -39,8 +39,8 @@ class Manta():
 		self.max_torque = 60
 		self.min_torque =0
 
-		self.max_velocity = 30
-		self.min_velocity = 0
+		self.max_accel = 40
+		self.min_accel = 0
 
 
 		
@@ -75,37 +75,46 @@ class Manta():
 		img = np.array(self.image, dtype = np.uint8)
 		img.resize([self.resolution[0], self.resolution[1], 3]) # 2 dimensiones, RGB, 256 x 128
 
-		return { # queda pendiente normalizar (linealmente, entre 0 y 1) todos los sensores
-			"steer_pos": self.norm(self.steer_pos, -self.max_steer_angle, self.max_steer_angle),
-			"velocity": self.norm(velocity, self.min_velocity, self.max_velocity),
+		# return { # queda pendiente normalizar (linealmente, entre 0 y 1) todos los sensores
+		# 	"steer_pos": self.norm(self.steer_pos, -self.max_steer_angle, self.max_steer_angle),
+		# 	"velocity": velocity,
 
-			"state_vl": 1 if state_vl else 0,
-			"point_vl": point_vl[2]/5 if state_vl else 1,
-			#"handle_vl": handle_vl,
-			"normal_vl": normal_vl,
+		# 	"state_vl": 1 if state_vl else 0,
+		# 	"point_vl": point_vl[2]/5 if state_vl else 1,
+		# 	#"handle_vl": handle_vl,
+		# 	"normal_vl": normal_vl,
 
-			"state_l": 1 if state_l else 0,
-			"point_l": point_l[2]/5 if state_l else 1,
-			#"handle_l": handle_l,
-			"normal_l": normal_l,
+		# 	"state_l": 1 if state_l else 0,
+		# 	"point_l": point_l[2]/5 if state_l else 1,
+		# 	#"handle_l": handle_l,
+		# 	"normal_l": normal_l,
 
-			"state_f": 1 if state_f else 0,
-			"point_f": point_f[2]/5 if state_f else 1,
-			#"handle_f": handle_f,
-			"normal_f": normal_f,
+		# 	"state_f": 1 if state_f else 0,
+		# 	"point_f": point_f[2]/5 if state_f else 1,
+		# 	#"handle_f": handle_f,
+		# 	"normal_f": normal_f,
 
-			"state_r": 1 if state_r else 0,
-			"point_r": point_r[2]/5 if state_r else 1,
-			#"handle_r": handle_r,
-			"normal_r": normal_r,
+		# 	"state_r": 1 if state_r else 0,
+		# 	"point_r": point_r[2]/5 if state_r else 1,
+		# 	#"handle_r": handle_r,
+		# 	"normal_r": normal_r,
 
-			"state_vr": 1 if state_vr else 0,
-			"point_vr": point_vr[2]/5 if state_vr else 1,
-			#"handle_vr": handle_vr,
-			"normal_vr": normal_vr,
+		# 	"state_vr": 1 if state_vr else 0,
+		# 	"point_vr": point_vr[2]/5 if state_vr else 1,
+		# 	#"handle_vr": handle_vr,
+		# 	"normal_vr": normal_vr,
 
-			"camera": img/255
-		}
+		# 	"camera": img/255
+		# }
+
+		return [img/255,np.asarray(
+						self.norm(self.steer_pos, -self.max_steer_angle, self.max_steer_angle), velocity,
+						1 if state_vl else 0, point_vl[2]/5 if state_vl else 1, normal_vl[0], normal_vl[1], normal_vl[2],
+						1 if state_l else 0, point_l[2]/5 if state_l else 1, normal_l[0], normal_l[1], normal_l[2],
+						1 if state_f else 0, point_f[2]/5 if state_f else 1, normal_f[0], normal_f[1], normal_f[2],
+						1 if state_r else 0, point_r[2]/5 if state_r else 1, normal_r[0], normal_r[1], normal_r[2],
+						1 if state_vr else 0, point_vr[2]/5 if state_vr else 1, normal_vr[0], normal_vr[1], normal_vr[2]
+						)]
 
 	def act(self, turn_discr, accel_discr): # entradas discretas, que hay que pasar a salidas continuas y no normalizadas
 
@@ -113,38 +122,34 @@ class Manta():
 		# accel_discr: string con estas posibilidades:  {"hard_break", "medium_break", "slight_break", "inertia", "slight_accel", "medium_accel", "full_gas"}
 
 		turn_dict = {
-			"very_left": 0, 
-			"left": 0.2,
-			"slight_left": 0.35,
-			"front": 0.5,
-			"slight_right": 0.65,
-			"right": 0.8,
-			"very_right": 1
+			"[1, 0, 0, 0, 0, 0, 0]": 0,   # "very_left"
+			"[0, 1, 0, 0, 0, 0, 0]": 0.2,  # "left"
+			"[0, 0, 1, 0, 0, 0, 0]": 0.35,  # "slight_left"
+			"[0, 0, 0, 1, 0, 0, 0]": 0.5,  # "front"
+			"[0, 0, 0, 0, 1, 0, 0]": 0.65,  # "slight_right"
+			"[0, 0, 0, 0, 0, 1, 0]": 0.8,  # "right"
+			"[0, 0, 0, 0, 0, 0, 1]": 1  # "very_right"
 		}
 
-		steer_denorm = self.denorm(turn_dict[turn_discr],-self.max_steer_angle,self.max_steer_angle)
+		steer_denorm = self.denorm(turn_dict[str(turn_discr)],-self.max_steer_angle,self.max_steer_angle)
 
 		vrep.simxSetJointTargetPosition(self.clientID, self.steer_handle, steer_denorm, vrep.simx_opmode_streaming)
 
-		if accel_discr == "hard_break":
-			brake_force = 1
-			motor_torque = 0
-			motor_velocity = 0
 
 		accel_dict = { # (brake_force,motor_torque,motor_velocity)
-			"hard_break": 	(1,		0,		0),
-			"medium_break": (0.5,	0,		0),
-			"slight_break": (0.25,	0,		0),
-			"inertia": 		(0,		0,		0),
-			"slight_accel": (0,		1,		0.25),
-			"medium_accel": (0,		1,		0.5),
-			"full_gas": 	(0,		1,		1)
+			"[1, 0, 0, 0, 0, 0, 0]": 	(1,		0,		0),   	#"hard_break"
+			"[0, 1, 0, 0, 0, 0, 0]": 	(0.5,	0,		0),   	#"medium_break"
+			"[0, 0, 1, 0, 0, 0, 0]": 	(0.25,	0,		0),   	#"slight_break"
+			"[0, 0, 0, 1, 0, 0, 0]": 	(0,		0,		0),   	#"inertia"
+			"[0, 0, 0, 0, 1, 0, 0]": 	(0,		1,		0.25),  #"slight_accel"
+			"[0, 0, 0, 0, 0, 1, 0]": 	(0,		1,		0.5),   #"medium_accel"
+			"[0, 0, 0, 0, 0, 0, 1]": 	(0,		1,		1)   	#"full_gas"
 		}
 
-		brake_force_norm, motor_torque_norm, motor_velocity_norm = accel_dict[accel_discr]
+		brake_force_norm, motor_torque_norm, motor_velocity_norm = accel_dict[str(accel_discr)]
 		brake_force = self.denorm(brake_force_norm, self.min_brake, self.max_brake)
 		motor_torque = self.denorm(motor_torque_norm, self.min_torque, self.max_torque)
-		motor_velocity = self.denorm(motor_velocity_norm, self.min_velocity, self.max_velocity)
+		motor_velocity = self.denorm(motor_velocity_norm, self.min_accel, self.max_accel)
 
 
 		vrep.simxSetJointForce(self.clientID, self.motor_handle, motor_torque, vrep.simx_opmode_streaming)
--- src/manta_main.py
+++ src/manta_main.py
@@ -3,10 +3,12 @@ import time
 import sys
 import numpy as np
 import signal
+
 from class_Manta import Manta
+from NeuralNet import DNN
 
 time_step = 0.5
-sim_time = 3
+sim_time = 20
 steps = int(sim_time/time_step)
 
 nb_sims = 5 # number of simulations were data is recolected before retraining the DNN
@@ -36,6 +38,8 @@ def firstConnection():
 
 clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5)
 
+dnn = DNN()
+
 for retrain in range(nb_dnn_retrains): # until convergence in reward, o otro criterio de parada
 	
 	for sim in range(nb_sims):
@@ -47,24 +51,26 @@ for retrain in range(nb_dnn_retrains): # until convergence in reward, o otro cri
 		sensors = manta.getSensors()
 
 
-		history = []
+		history_sensors = []
+		history_actions = []
 
 		for step in range(steps):
 
 			sensors = manta.getSensors()
 			
-			## dnn_out = DNN(sensors) # get probability of each action
+			dnn_out = dnn.evaluar(sensors) # get probability of each action
 
-			## action = choose_action(dnn_out) # randomly choose with the previously gotten probabilities
+			action = choose_action(dnn_out) # randomly choose with the previously gotten probabilities
 
-			#history.append((sensors, action))
+			history_sensors.append(sensors)
+			history_actions.append(actions)
 
-			manta.act("front", "slight_accel")#manta.act(*action)
+			manta.act(*action)
 
 			time.sleep(time_step) # asegurarse de que es correcto hacer esto, de que no estamos peridendo
 								  # la mayoria del tiempo en computar la DNN y historias
 
-		## reward = setReward(history)
+		reward = reward_simple(history_sensors)
 
 		## new_training_labels = getLabels(history, reward)
 
@@ -78,10 +84,57 @@ for retrain in range(nb_dnn_retrains): # until convergence in reward, o otro cri
 
 
 	## retrain_DNN(training_labels)
-	print("ahora deberia de estar paradao")
+	print("ahora deberia de estar parado")
 	time.sleep(5)
 	print("Ya no")
 
+def reward_simple(history):
+	luzera = len(history)
+	v = 0
+	for i in range(luzera):
+		v += history[i][1][1]
+	return v/luzera
+
+
+def choose_action(dnn_out):
+	# dnn_out es un np.array, que represneta dos distribuciones de probabilidad
+	p_turn = dnn_out[0:7] # coger las probabilidades que tienen que ver con la direccion
+	p_accel = dnn_out[7:14] # coger las probabilidades que tienen que ver con la accel/break
+
+	turn_ind = np.random.choice[7,1,p=p_turn/np.sum(p_turn)]
+	accel_ind = np.random.choice[7,1,p=p_accel/np.sum(p_accel)]
+
+	turn_act = [0]*7
+	accel_act = [0]*7
+
+	turn_act[turn_ind] = 1
+	accel_act[accel_ind] = 1
+
+	return turn_act, accel_act
+
+
+def getLabels(history,reward,a,b,c,d):
+	labels = [] [sensors,]
+
+
+
+	return labels
+
+
+
+
+
+
+def reward_curvatura(history):
+	luzera = len(history)
+	v = 0
+	for i in range(luzera):
+		v += history[i][0]["velocity"]
+	
+
+	## computar parte de la curvatura
+
+	return(f(v/luzera, zailtasuna))
 
 
 
